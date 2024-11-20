0d3707117d0800161ddf9f22b4403549ed8ba06a
IPicahy <168136292+IPicahy@users.noreply.github.com>
2024-11-20 11:09:11 +0500

    Update 20.11.2024.py

--- 20.11.2024.py
+++ 20.11.2024.py
@@ -1,23 +1,15 @@
 inputfile_one = "INN.csv"
 data = []
 seek_data = input("Введите номер или название объекта про которого хотите найти информацию: ").strip()
-
+arr = []
 with (open(inputfile_one, "r", encoding="utf-8") as file):
     for l in file:
         row = l.strip().split(";")
-
-        for i in row[2]:
-            if seek_data in i and not i == "":
-                ind = row[0]
-                print(f"Вы ввели название этого объекта: {row[0]}-{row[1]}-{row[2]} ({row[3]})")
-
-        if row[2] == seek_data:
-            ind = row[0]
-            print(f"Вы ввели номер этого объекта: {row[0]}-{row[1]}-{row[2]} ({row[3]})")
-
-with (open(inputfile_one, "r", encoding="utf-8") as file):
-    for l in file:
-        row = l.strip().split(";")
-        if ind==row[1]:
-            print(f"{row[0]}-{row[1]}-{row[2]} ({row[3]})")
-
+        arr.append(row)
+for i in arr:
+    if seek_data == i[2]:
+        ind = i[0]
+        print(f"{ind}")
+for i in arr:
+    if i[1] == ind:
+        print(f"Вы ввели номер этого объекта: {i[3]}")
